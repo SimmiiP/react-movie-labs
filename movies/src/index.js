bc45726640aab1137fd5a4f9e63412ffbd72bde9
Simi <simi.popola@gmail.com>
2022-03-08 11:13:20 +0000

    Refractor: Favorite movie list now stored in a context.

--- movies/src/index.js
+++ movies/src/index.js
@@ -9,6 +9,7 @@ import SiteHeader from './components/siteHeader'
 import MovieUpcomingPage from './pages/movieUpcomingPage';
 import {QueryClientProvider, QueryClient } from "react-query";
 import {ReactQueryDevtools} from 'react-query/devtools'
+import MoviesContextProvider from "./contexts/moviesContext";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -25,6 +26,7 @@ const App = () => {
     <QueryClientProvider client={queryClient}>
     <BrowserRouter>
       <SiteHeader />
+      <MoviesContextProvider>
       <Routes>
         <Route path="/movies/upcoming" element={ <MovieUpcomingPage />} />
         <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
@@ -33,6 +35,7 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={ <Navigate to="/" /> } />
       </Routes>
+      </MoviesContextProvider>
     </BrowserRouter>
     <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
