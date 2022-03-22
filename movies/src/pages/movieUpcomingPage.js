6bb7a3f52ed491952c322d3e4d9acb8e20a1533c
Simi <simi.popola@gmail.com>
2022-03-22 11:03:03 +0000

    Change upcoming movies page to use caching

--- movies/src/pages/movieUpcomingPage.js
+++ movies/src/pages/movieUpcomingPage.js
@@ -1,31 +1,30 @@
 import React, { useState, useEffect } from "react";
 import PageTemplate from '../components/templateMovieListPage'
 import { getupcomingMovies } from "../api/tmdb-api";
+import { useQuery } from 'react-query';
+import Spinner from '../components/spinner';
 import AddToPlaylistIcon from '../components/cardIcons/addToPlaylist';
 
 const UpcomingMoviePage = (props) => {
-  const [movies, setMovies] = useState([]);
-  const favorites = movies.filter(m => m.favorite)
-  localStorage.setItem('favorites', JSON.stringify(favorites))
+  const {  data, error, isLoading, isError }  = useQuery('upcoming', getupcomingMovies)
 
-  const addToFavorites = (movieId) => {
-    const updatedMovies = movies.map((m) =>
-      m.id === movieId ? { ...m, favorite: true } : m
-    );
-    setMovies(updatedMovies);
-  };
+  if (isLoading) {
+    return <Spinner />
+  }
 
-  useEffect(() => {
-    getupcomingMovies().then(movies => {
-      setMovies(movies);
-    });
-  }, []);
+  if (isError) {
+    return <h1>{error.message}</h1>
+  } 
+ const movies = data.results
+  
+  
+
+  
 
   return (
     <PageTemplate
       title='Upcoming Movies'
       movies={movies}
-      selectFavorite={addToFavorites}
       action={(movie) => {
         return (
           <>
